31996907e93c8db8516ddaee44887a9265422128
Daniel Dyer <danielrd@mac.com>
2013-11-04 00:11:27 +1100

    Switched to new dynamics model (hardcoded parameters for now)

--- src/nmpc/dynamics.cpp
+++ src/nmpc/dynamics.cpp
@@ -70,158 +70,107 @@ Runs a basic fixed-wing flight dynamics model, calculating:
 
 AccelerationVector FixedWingFlightDynamicsModel::evaluate(
 const State &in, const ControlVector &control) const {
-	/* Cache state data for convenience */
-	Quaternionr attitude = Quaternionr(in.attitude());
-	real_t yaw_rate = in.angular_velocity()[2],
-		  pitch_rate = in.angular_velocity()[1],
-		  roll_rate = in.angular_velocity()[0];
+   /* Cache state data for convenience */
+   Quaternionr attitude = Quaternionr(in.attitude());
+   real_t yaw_rate = in.angular_velocity()[2],
+          pitch_rate = in.angular_velocity()[1],
+          roll_rate = in.angular_velocity()[0];
 
-	/* External axes */
-	Vector3r airflow, lift_axis, drag_axis, side_axis;
-	real_t v, v_inv;
+   /* External axes */
+   Vector3r airflow;
+   real_t v, v_inv, horizontal_v2, vertical_v2;
 
-	airflow = attitude * (in.wind_velocity() - in.velocity());
-	v = airflow.norm();
+   airflow = attitude * (in.wind_velocity() - in.velocity());
+   v = airflow.norm();
+   horizontal_v2 = airflow.y() * airflow.y() + airflow.x() * airflow.x();
+   vertical_v2 = airflow.z() * airflow.z() + airflow.x() * airflow.x();
 
-	if (v < NMPC_DYNAMICS_MIN_V) {
-	   /*
-	   Airflow too slow for any of this to work; just return acceleration
-	   due to gravity
-	   */
-	   AccelerationVector output;
-	   output.segment<3>(0) = attitude * Vector3r(0, 0, G_ACCEL);
+   if (horizontal_v2 < NMPC_DYNAMICS_MIN_V) {
+       /*
+       Airflow too slow for any of this to work; just return acceleration
+       due to gravity
+       */
+       AccelerationVector output;
+       output.segment<3>(0) = attitude * Vector3r(0, 0, G_ACCEL);
 
-	   /* No angular acceleration */
-	   output.segment<3>(3) << 0, 0, 0;
-	   return output;
-	}
+       /* No angular acceleration */
+       output.segment<3>(3) << 0, 0, 0;
+       return output;
+   }
 
-	v_inv = (real_t)1.0 / v;
+   v_inv = (real_t)1.0 / v;
 
-	/* Determine alpha and beta: alpha = atan(wz/wx), beta = atan(wy/|wxz|) */
-	real_t alpha, beta, qbar, a, b, a2, b2;
-	qbar = RHO * v * v * (real_t)0.5;
-	alpha = std::atan2(-airflow.z(), -airflow.x());
-	beta = std::asin(airflow.y() * v_inv);
+   /* Determine alpha and beta: alpha = atan(wz/wx), beta = atan(wy/|wxz|) */
+   real_t alpha, beta, qbar, sin_alpha, cos_alpha, sin_beta, cos_beta,
+          lift, drag, side_force, roll_moment, pitch_moment, yaw_moment,
+          a2;
+   qbar = RHO * horizontal_v2 * (real_t)0.5;
+   alpha = std::atan2(-airflow.z(), -airflow.x());
+   beta = std::asin(airflow.y() * v_inv);
 
-	/*
-	Clamp alpha to [-60, 60] for the purposes of calculating lift and drag
-	*/
-	if (alpha > M_PI/2.0) {
-	   a = M_PI - alpha;
-	} else if (alpha < -M_PI/2.0) {
-	   a = -M_PI - alpha;
-	} else {
-	   a = alpha;
-	}
-	b = beta;
+   sin_alpha = std::sin(alpha);
+   cos_alpha = std::cos(alpha);
+   sin_beta = std::sin(beta);
+   cos_beta = std::cos(beta);
 
-	a2 = a * a;
-	b2 = b * b;
+   a2 = alpha * alpha;
 
-	/*
-	Set up a^4, a^3, a^2, a, 1 so that we can use dot product for the
-	polynomial evaluation.
-	*/
-	Vector5r alpha_coeffs;
-	alpha_coeffs << a2 * a2, a2 * a, a2, a, 1.0;
+   lift = -5 * a2 * alpha + a2 + 2.5 * alpha + 0.12;
+   if (alpha < 0.0) {
+       lift = std::min(lift, 0.8 * sin_alpha * cos_alpha);
+   } else {
+       lift = std::max(lift, 0.8 * sin_alpha * cos_alpha);
+   }
 
-	/* Evaluate quartics in alpha to determine lift and drag */
-	real_t lift = alpha_coeffs.dot(c_lift_alpha),
-	      drag = alpha_coeffs.dot(c_drag_alpha);
+   drag = 0.05 + 0.7 * sin_alpha * sin_alpha + 0.1 * sin_beta * sin_beta;
+   side_force = 0.3 * sin_beta;
 
-	/*
-	Assume lift is somewhat well-behaved for alpha in the range [-0.25, 0.25].
-	If outside that range, clamp it to 0 so that the polynomial doesn't have
-	to model the full possible range.
-	*/
-	if (std::abs(alpha) > 1.0) {
-	   lift = 0.0;
-	} else if (alpha > 0.25) {
-	   lift = std::max(lift, 0.0);
-	} else if (alpha < -0.25) {
-	   lift = std::min(lift, 0.0);
-	}
+   pitch_moment = 0.001 - 0.1 * sin_alpha * cos_alpha - 0.003 * pitch_rate -
+                  0.01 * control[1] - 0.01 * control[2];
+   roll_moment = -0.015 * roll_rate + 0.025 * control[1] - 0.025 * control[2];
+   yaw_moment = -0.02 * sin_beta - 0.05 * yaw_rate -
+                0.01 * std::abs(control[1]) + 0.01 * std::abs(control[2]);
 
-	/*
-	Determine motor thrust and torque:
-	https://www.grc.nasa.gov/WWW/Wright/airplane/propth.html
+   /*
+   Determine motor thrust and torque:
+   https://www.grc.nasa.gov/WWW/Wright/airplane/propth.html
 
-	Propeller thrust =
-	F = 0.5 * rho * A * (Ve^2 - V0^2)
-	where A = propeller disc area, Ve = exit velocity, V0 = air velocity
+   Propeller thrust =
+   F = 0.5 * rho * A * (Ve^2 - V0^2)
+   where A = propeller disc area, Ve = exit velocity, V0 = air velocity
 
-	In this formulation,
-	F = 0.5 * rho * A * ((k * rpm)^2 - V0^2)
+   In this formulation,
+   F = 0.5 * rho * A * ((k * rpm)^2 - V0^2)
 
-	Presumably the relationship between thrust and torque on airframe is
-	constant? If so, they're related by prop_ct.
-	*/
-	real_t thrust = 0.0;
-	if (motor_idx < NMPC_CONTROL_DIM) {
-	   real_t ve = prop_cve * control[motor_idx], v0 = airflow.x();
-	   thrust = (real_t)0.5 * RHO * prop_area * (ve * ve - v0 * v0);
-	   if (thrust < 0.0) {
-	       /* Folding prop, so assume no drag */
-	       thrust = 0.0;
-	   }
-	}
+   Presumably the relationship between thrust and torque on airframe is
+   constant? If so, they're related by prop_ct.
+   */
+   real_t thrust = 0.0;
+   if (motor_idx < NMPC_CONTROL_DIM) {
+       real_t ve = prop_cve * control[motor_idx], v0 = airflow.x();
+       thrust = (real_t)0.5 * RHO * prop_area * (ve * ve - v0 * v0);
+       if (thrust < 0.0) {
+           /* Folding prop, so assume no drag */
+           thrust = 0.0;
+       }
+   }
 
-	/*
-	Set up the side force coefficient vector, and calculate side force
-	*/
-	Vector4r side_coeffs;
-	side_coeffs << b2, b, yaw_rate, roll_rate;
+   /*
+   Sum and apply forces and moments
+   */
+   Vector3r sum_force, sum_torque;
+   sum_force << thrust + qbar * (lift * sin_alpha - drag * cos_alpha - side_force * sin_beta),
+                qbar * side_force * cos_beta,
+                -qbar * (lift * cos_alpha + drag * sin_alpha);
+   sum_torque = qbar * Vector3r(roll_moment, pitch_moment, yaw_moment);
 
-	real_t side_force = side_coeffs.dot(c_side_force) +
-	   control.dot(c_side_force_control);
+   /* Calculate linear acceleration (F / m) */
+   AccelerationVector output;
+   output.segment<3>(0) = sum_force * mass_inv +
+                          attitude * Vector3r(0, 0, G_ACCEL);
 
-	/*
-	Calculate pitch moment
-	*/
-	Vector2r pitch_coeffs;
-	pitch_coeffs << a,
-	   pitch_rate * (pitch_rate < 0.0 ? -pitch_rate : pitch_rate);
+   /* Calculate angular acceleration (tau / inertia tensor) */
+   output.segment<3>(3) = inertia_tensor_inv * sum_torque;
 
-	real_t pitch_moment = pitch_coeffs.dot(c_pitch_moment) +
-	   control.dot(c_pitch_moment_control);
-
-	/*
-	Roll moment
-	*/
-	Vector1r roll_coeffs;
-	roll_coeffs << roll_rate;
-	real_t roll_moment = roll_coeffs.dot(c_roll_moment) +
-	   control.dot(c_roll_moment_control);
-
-	/*
-	Yaw moment
-	*/
-	Vector2r yaw_coeffs;
-	yaw_coeffs << b, yaw_rate;
-	real_t yaw_moment = yaw_coeffs.dot(c_yaw_moment) +
-	   control.dot(c_yaw_moment_control);
-
-	/*
-	Sum and apply forces and moments
-	*/
-	Vector3r sum_force, sum_torque;
-
-	sum_force = qbar * (
-	       (lift * std::cos(alpha) + drag * std::sin(alpha)) * Vector3r(0, 0, -1) +
-	       (lift * std::sin(alpha) - drag * std::cos(alpha) - side_force * std::sin(beta)) * Vector3r(1, 0, 0) +
-	       side_force * std::cos(beta) * Vector3r(0, 1, 0)
-	   ) +
-	   thrust * Vector3r(1, 0, 0);
-	sum_torque = qbar * Vector3r(roll_moment, pitch_moment, yaw_moment);
-
-	/* Calculate linear acceleration (F / m) */
-	AccelerationVector output;
-	output.segment<3>(0) = sum_force * mass_inv +
-	                      attitude * Vector3r(0, 0, G_ACCEL);
-
-	/* Calculate angular acceleration (tau / inertia tensor) */
-	output.segment<3>(3) = inertia_tensor_inv * sum_torque;
-
-	return output;
+   return output;
 }
