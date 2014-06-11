36739cf2f499c87bb3cb487f994afb45af4e7d75
Ben Dyer <ben.dyer@taguchimail.com>
2014-06-11 21:48:10 +1000

    Updated for test flight 10

--- ccs-c66x/cnmpc.c
+++ ccs-c66x/cnmpc.c
@@ -440,10 +440,10 @@ const real_t *restrict state, const real_t *restrict control) {
     real_t lift, drag, side_force;
 
     /* 0.26315789473684 is the reciprocal of mass (3.8kg) */
-    lift = (qbar * 0.26315789473684f) * (0.7f * sin_cos_alpha + 0.05f);
+    lift = (qbar * 0.26315789473684f) * (0.7f * sin_cos_alpha + 0.1f);
     drag = (qbar * 0.26315789473684f) *
            (0.05f + 0.7f * sin_alpha * sin_alpha);
-    side_force = (qbar * 0.26315789473684f) * 0.1f * sin_beta * cos_beta;
+    side_force = (qbar * 0.26315789473684f) * 0.05f * sin_beta * cos_beta;
 
     /* Convert aerodynamic forces from wind frame to body frame */
     real_t x_aero_f = lift * sin_alpha - drag * cos_alpha +
@@ -468,11 +468,11 @@ const real_t *restrict state, const real_t *restrict control) {
            left_aileron = control[1] - 0.5f,
            right_aileron = control[2] - 0.5f;
     pitch_moment = 0.0f - 0.0f * sin_alpha - 0.0f * pitch_rate -
-                   0.2f * (left_aileron + right_aileron) * vertical_v * 0.1f;
-    roll_moment = 0.05f * sin_beta - 0.1f * roll_rate +
-                  0.45f * (left_aileron - right_aileron) * vertical_v * 0.1f;
-    yaw_moment = 0.05f * sin_beta - 0.1f * yaw_rate -
-                 0.05f * (absval(left_aileron) - absval(right_aileron)) *
+                   0.1f * (left_aileron + right_aileron) * vertical_v * 0.1f;
+    roll_moment = 0.05f * sin_beta - 0.05f * roll_rate +
+                  0.2f * (left_aileron - right_aileron) * vertical_v * 0.1f;
+    yaw_moment = 0.025f * sin_beta - 0.025f * yaw_rate -
+                 0.15f * (absval(left_aileron) - absval(right_aileron)) *
                  vertical_v * 0.1f;
     pitch_moment *= qbar;
     roll_moment *= qbar;
