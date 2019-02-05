a868a8dd958d5a67e00be60b361d44edfa79c452
Daniel Dyer <danielrd@mac.com>
2019-02-06 00:34:51 +1100

    Minor optimisations

--- support/MATLAB/benchmark/bench_dynamics.m
+++ support/MATLAB/benchmark/bench_dynamics.m
@@ -1,8 +1,6 @@
 % System dynamics model. Takes a state vector and a control vector, and
 % returns the state vector derivative.
 function xdot = bench_dynamics(x, u)
-    xdot = zeros(size(x));
-
     % State vector:
     %     - x(1): x-axis position
     %     - x(2): y-axis position
@@ -13,7 +11,9 @@ function xdot = bench_dynamics(x, u)
     % Control vector:
     %     - u(1): angular velocity
     %     - u(2): forward acceleration
-    xdot(1:2) = x(3:4);
-    xdot(3:4) = [cos(x(5)); sin(x(5))] * u(2);
-    xdot(5) = u(1);
+    xdot = [...
+        x(3:4);
+        [cos(x(5)); sin(x(5))] * u(2);
+        u(1);
+    ];
 end
